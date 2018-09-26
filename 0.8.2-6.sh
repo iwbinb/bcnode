d8686bc5b6f62f74ace61a2480f91c79136d3fb4
iwbinb <34509975+iwbinb@users.noreply.github.com>
2018-09-26 09:51:54 +0800

    Update 0.8.2-6.sh

--- 0.8.2-6.sh
+++ 0.8.2-6.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --restart unless-stopped --name bcnode -d --cpus 0.8 -p 3000:3000 -p 16061:16061 -p 16060:16060 -e BC_DISCOVERY_MDNS=‘true’ blockcollider/bcnode:0.8.2 start --ws --rovers --ui --node --miner-key 0x752c2f42dff1c69a1275cae45b404fc182fa6331
+docker run --restart unless-stopped --name bcnode -d --cpus 0.75 -p 3000:3000 -p 16061:16061 -p 16060:16060 -e BC_DISCOVERY_MDNS=‘true’ blockcollider/bcnode:0.8.2 start --ws --rovers --ui --node --miner-key 0x752c2f42dff1c69a1275cae45b404fc182fa6331
