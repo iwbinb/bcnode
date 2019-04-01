025d2bb5508a048d39430ec01e54638986482638
iwbinb <34509975+iwbinb@users.noreply.github.com>
2019-04-02 00:58:21 +0800

    Update 0.8.2-5.sh

--- 0.8.2-5.sh
+++ 0.8.2-5.sh
@@ -1,2 +1,23 @@
 #!/bin/sh
-docker run --restart unless-stopped --name bcnode -d --cpus 0.75 -p 3000:3000 -p 16061:16061 -p 16060:16060 -e BC_DISCOVERY_MDNS=‘true’ blockcollider/bcnode:0.8.2 start --ws --rovers --ui --node --miner-key 0x6be985e15a1652621f715b4fbffad05f4b4638b8
+sudo su
+cd /home/ubuntu
+sudo git clone https://github.com/n-y-z-o/nyzoVerifier.git
+cd nyzoVerifier
+sudo ./gradlew build
+sudo mkdir -p /var/lib/nyzo/production
+sudo cp trusted_entry_points /var/lib/nyzo/production
+sudo cp nyzoVerifier.conf /etc/supervisor/conf.d
+sudo bash -c 'echo Zillipa$RANDOM > /var/lib/nyzo/production/nickname'
+sudo supervisorctl reload
+sleep 2s
+sudo renice -n -5 -p $(pgrep ^java$)
+echo "@reboot sudo supervisorctl reload" >> mycron
+sudo crontab mycron
+rm mycron
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo supervisorctl status
+more /var/lib/nyzo/production/verifier_private_seed
